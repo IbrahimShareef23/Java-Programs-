4d9df1427f090d0242b96062ece231767f91e68b
IbrahimShareef23 <65616062+IbrahimShareef23@users.noreply.github.com>
2021-02-23 17:07:52 +0530

    Add files via upload

---
+++ AreaofRect.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class info
+{
+int b,h;
+}
+class AreaofRect
+{
+public static void main(String args[])
+{
+double a;
+info i =new info();
+i.b=10;
+i.h=20;
+a=0.5* i.b* i.h;
+
+System.out.println(a);
+}
+}

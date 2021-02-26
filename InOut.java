5933cbecd51f9d57bdfbb57dfcb507f751def1fb
IbrahimShareef23 <65616062+IbrahimShareef23@users.noreply.github.com>
2021-02-26 19:55:55 +0530

    Add files via upload

---
+++ InOut.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class InOut
+{
+public static void main(String args[])
+{
+Scanner s =new Scanner(System.in);
+System.out.println("enter the name of the customer:");
+String w=s.nextLine();
+System.out.println("the entered word was:"+w);
+System.out.println("enter the Mobile Number:");
+long no=s.nextLong();
+System.out.println("the Phone no is :"+no);
+
+}
+}

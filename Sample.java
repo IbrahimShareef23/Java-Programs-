ade74921b6a13ea771db633a4dbf0d486c528ab9
IbrahimShareef23 <65616062+IbrahimShareef23@users.noreply.github.com>
2021-02-27 19:35:11 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sample
+{
+public static void main(String args[])
+{
+Sample s= new Sample();
+Scanner z =new Scanner(System.in);
+System.out.println("enter a number to be stored");
+z.nextInt();
+
+}
+}

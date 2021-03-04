a21114827bafb1d11bf3018fa94cdd33e5909397
IbrahimShareef23 <65616062+IbrahimShareef23@users.noreply.github.com>
2021-03-04 23:57:07 +0530

    Add files via upload

---
+++ OperatorsJava.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class OperatorsJava
+{
+public static void main (String[] arg)
+{
+int i=10;
+i-=-7;
+System.out.println("the value of int is:"+i);
+}
+}
+
+//i+=3
+//i-=4
+//i=i+4 or i+=4
+//i=i-4 or i-=4
+//i=

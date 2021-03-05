f05ad38672221a7c17082d5f4a4c7872acdd07a4
IbrahimShareef23 <65616062+IbrahimShareef23@users.noreply.github.com>
2021-03-05 19:21:37 +0530

    Create TernaryOp.java

---
+++ TernaryOp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TernaryOp
+{
+Public static void main(String[]
+arg)
+{
+  int a=10;
+  int b=20;
+  int max;
+  max=(b>a)?b:a;
+  System.out.println("the max value is:"+max);
+}
+}
